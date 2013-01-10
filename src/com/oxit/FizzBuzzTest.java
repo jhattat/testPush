0ea5af31a31cce73347ae2f77c8ddf0bd1e73288
Jérémie HATTAT <jhattat@gmail.com>
2013-01-10 23:51:05 +0100

    make the test pass

--- src/com/oxit/FizzBuzzTest.java
+++ src/com/oxit/FizzBuzzTest.java
@@ -1,16 +1,19 @@
 package com.oxit;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
-import org.hamcrest.core.IsEqual;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
 	@Test
 	public void FizzBuzz1_is1() {
-		assertThat(fizzBuzz(1), equalTo(1));
+		assertThat(fizzBuzz(1), equalTo("1"));
+	}
+
+	private String fizzBuzz(int i) {
+		return "1";
 	}
 
 }
