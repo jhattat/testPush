19273e9b475349d5bb3a0d731807657e53869cad
Jérémie HATTAT <jhattat@gmail.com>
2013-01-10 23:50:09 +0100

    Test KO

--- src/com/oxit/FizzBuzzTest.java
+++ src/com/oxit/FizzBuzzTest.java
@@ -1,14 +1,16 @@
 package com.oxit;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+import org.hamcrest.core.IsEqual;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
 	@Test
-	public void testAreConfigured() {
-		assertTrue(true);
+	public void FizzBuzz1_is1() {
+		assertThat(fizzBuzz(1), equalTo(1));
 	}
 
 }
